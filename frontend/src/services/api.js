f12f4bdfbc3b45ba52bcceebc22fce5d8dee9a61
AaronAnteCamacho <negertion@gmail.com>
2026-04-14 10:46:38 -0700

    Conectar frontend con backend de Azure

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+const API_URL = import.meta.env.VITE_API_URL || "https://gestor-hospitalario-api-czgmrgtbshuace6.mexicocentral-01.azurewebsites.net";
 
 function getToken() {
   // ✅ 1) primero intenta token separado
